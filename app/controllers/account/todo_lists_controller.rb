7251074230ac79bb9490b3b998bdec0a2ba3c025
milesmao233 <milesmao233@gmail.com>
2017-03-03 15:21:32 +0800

    finish this project

--- app/controllers/account/todo_lists_controller.rb
+++ app/controllers/account/todo_lists_controller.rb
@@ -3,7 +3,7 @@ class Account::TodoListsController < ApplicationController
   before_action :set_todo_list, only:[:show, :edit, :update, :destroy]
 
   def index
-    @todo_lists = current_user.todo_lists.order("created_at DESC")
+    @todo_lists = current_user.todo_lists.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
